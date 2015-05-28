1eafada2af50cc71a2912f7ab7163116bea42c49
Mathieu Blond <mathieu.blond@ijinus.fr>
2015-05-28 16:56:54 +0200

    Each file is now licensed under LGPL 3.0 license.

--- src/com/ijinus/cs2j/xmleditor/javaFXui/MainWindow.java
+++ src/com/ijinus/cs2j/xmleditor/javaFXui/MainWindow.java
@@ -1,22 +1,40 @@
-package com.ijinus.cs2j.xmleditor.javaFXui;
+/*
+ Copyright 2015 Mathieu Blond - Ijinus ( mathieu.blond@ijinus.fr )
 
-import java.io.IOException;
+ This file is part of CS2JXmlEditor.
+
+    CS2JXmlEditor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CS2JXmlEditor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with CS2JXmlEditor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ijinus.cs2j.xmleditor.javaFXui;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import com.ijinus.cs2j.xmleditor.xml.XmlSerialization;
 import com.ijinus.cs2j.xmleditor.xml.model.InterfaceRepTemplate;
 
-
+/**
+ * 
+ * @author Mathieu Blond - Ijinus (http://www.ijinus.com/?lang=en)
+ *
+ * The main class of CS2JXmlEditor. Launches the software in a javaFX window.
+ *
+ */
 public class MainWindow extends Application{
 	
 	public static XmlSerialization serializer = new XmlSerialization();
